dcd819144b366cbc33fcc3f31190c1fb48863094
Xavi Ibáñez Martínez <241xavi@gmail.com>
2021-03-14 12:42:00 +0100

    Update CategoryController.cs
    Get category from ddbb to controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplicationNET.Data;
+using WebApplicationNET.Models;
 
 namespace WebApplicationNET.Controllers
 {
     public class CategoryController : Controller
     {
+        //Get all category from database
+        //Incializamos la bbdd en nuestro controller
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //Lista de categorias mapeadas
+            IEnumerable<Category> objList = _db.Category;
+
+            //Lo enviamos a la view por parametro
+            return View(objList);
         }
     }
 }
