37fc06566ab19b8f6f78a225deb849b4be42f2a7
Xavi Ibáñez Martínez <241xavi@gmail.com>
2021-03-18 21:59:30 +0100

    Application Type

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,11 +9,14 @@ namespace WebApplicationNET.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        //Conexion a la bbdd
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
 
+        //Setter y Getters que usamos para recuperar datos de la bbdd
         public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
     }
 }
